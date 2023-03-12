b3e7c994968a81d3c593c5e8dde80d275d45231c
Jedoku <vchulakv@gmail.com>
2023-03-12 20:02:14 +0200

    RS-8 Rubocop issues

--- chat.rb
+++ chat.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 require_relative 'member'
 require_relative 'message'
 
+# Description/Explanation of Chat class
 class Chat
   attr_accessor :members, :messages
   attr_reader :admins
@@ -9,27 +12,28 @@ class Chat
     @members = members
     @messages = messages
     @admins = admins
-  end  
-
-  def status
-    @members.each do |member| 
-      status_change(member) 
-      puts member.name + "': " + member.status
-    end 
   end
 
-  private 
-   
-  def status_change(member)
-    if member.messages.empty?
-      member.status = "passive"
-    else      
-      member.status = "active"
+  def status
+    @members.each do |member|
+      status_change(member)
+      puts "#{member.name}:#{member.status}"
     end
+  end
+
+  private
+
+  def status_change(member)
+    member.status = if member.messages.empty?
+                      'passive'
+                    else
+                      'active'
+                    end
   end
 end
 
-members = [Member.new('Xiollx'), Member.new('$anya'), Member.new('yurec'), Member.new('Yarik Jones'), Member.new(' '), Member.new('Bogdanium')]
+members = [Member.new('Xiollx'), Member.new('$anya'), Member.new('yurec'), Member.new('Yarik Jones'), Member.new(' '),
+           Member.new('Bogdanium')]
 
 chat = Chat.new(members, {}, members[0])
 
--- message.rb
+++ message.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
+# Description/Explanation of Chat class
 class Messege
   def initialize(text, member, chat)
     @text = text
     @member = member
     @chat = chat
   end
-end    
+end
